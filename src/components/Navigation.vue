20059922dee9005b3053acaf3706a817c422a3ba
Sebastien <sebastien.fillion.1@ulaval.ca>
2019-02-02 19:23:04 -0500

    Add navbar v1.0 with Bootstrap Vue and Font Awesome icon

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,17 +1,35 @@
 <template>
-    <nav>
-        <ul>
-            <li>
-                <router-link to="/">Home</router-link>
-            </li>
-            <li>
-                <router-link to="/album">Album</router-link>
-            </li>
-            <li>
-                <router-link to="/artist">Artist</router-link>
-            </li>
-        </ul>
-    </nav>
+  <b-navbar toggleable="md" type="dark" variant="primary">
+
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+    <b-navbar-brand href="#/">UBEAT</b-navbar-brand>
+
+    <b-collapse is-nav id="nav_collapse">
+
+      <b-navbar-nav>
+        <b-nav-item href="#/Album">Artist</b-nav-item>
+        <b-nav-item href="#/Artist">Album</b-nav-item>
+        <b-nav-item href="#/">Reading List</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit"><font-awesome-icon icon="search"/></b-button>
+        </b-nav-form>
+        <b-nav-item-dropdown right>
+          <template slot="button-content">
+            <em>Welcome user1</em>
+          </template>
+          <b-dropdown-item href="#/">Profile</b-dropdown-item>
+          <b-dropdown-item href="#/">Signout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
+library.add(faSearch);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
