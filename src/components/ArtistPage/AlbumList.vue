94a10f817e725e3584b4157df23f577d61f1fb5d
Sebastien <sebastien.fillion.1@ulaval.ca>
2019-03-17 20:16:36 -0400

    CSS change by @Dohai3

--- src/components/ArtistPage/AlbumList.vue
+++ src/components/ArtistPage/AlbumList.vue
@@ -3,10 +3,10 @@
     <b-list-group>
       <b-list-group-item class="albumLine"  v-for="item in albumList" v-bind:key="item.collectionId" v-on:click="loadAlbumPage(item.collectionId)">
         <b-img
-        class="albumImg"
-        v-bind:src= item.artworkUrl100
-        width="150"
-        height="150"/>
+          class="albumImg"
+          v-bind:src= item.artworkUrl100
+          width="150"
+          height="150"/>
         <ul class="list-unstyled albumInfos">
           <li><h4>{{ item.collectionName }}</h4></li>
           <li class="someInfos">{{ item.releaseDate.substring(0,4) }}</li>
@@ -38,10 +38,9 @@
         });
       },
       compareAlbums(a, b) {
-      // Use toUpperCase() to ignore character casing
+        // Use toUpperCase() to ignore character casing
         const genreA = a.releaseDate.substring(0, 4).toUpperCase();
         const genreB = b.releaseDate.substring(0, 4).toUpperCase();
-
         let comparison = 0;
         if (genreA < genreB) {
           comparison = 1;
@@ -66,7 +65,6 @@
     margin-top: 30px;
     margin-bottom: 60px;
   }
-
   .albumLine {
     background-color: lightskyblue;
     display: flex;
@@ -75,17 +73,47 @@
     cursor: pointer;
     background-color: #b7e0ff;
   }
-
   .albumInfos {
     color: black;
   }
-
   .albumImg {
     margin-right: 30px;
   }
-
   .someInfos {
     font-size: 12px;
   }
-
+  /* Small devices (Portrait phones, 576px and up) */
+  @media only screen and (min-width: 320px) and (max-width: 768px) {
+    #bottomDiv {
+      margin-top: 10px;
+    }
+    .albumImg {
+      margin-right: 10px;
+      width: 100px;
+      height: 100px;
+    }
+    .someInfos {
+      font-size: 12px;
+    }
+    h4 {
+      font-size: 16px;
+    }
+  }
+  /* Medium devices (tablets, 768px and up) */
+  @media only screen and (min-width: 768px) and (max-width: 1024px) {
+    #bottomDiv {
+      margin-top: 15px;
+    }
+    .albumImg {
+      margin-right: 20px;
+      width: 125px;
+      height: 125px;
+    }
+    .someInfos {
+      font-size: 14px;
+    }
+    h4 {
+      font-size: 18px;
+    }
+  }
 </style>
--- src/components/ArtistPage/Artist.vue
+++ src/components/ArtistPage/Artist.vue
@@ -24,83 +24,11 @@
 </script>
 
 <style>
-.mainDiv {
-  margin: 10px;
-}
-
-.mainDiv {
-  margin-top: 20px;
-  padding: auto;
-}
-/* Small devices (Portrait phones, 576px and up) */
-@media only screen and (min-width: 320px) and (max-width: 480px) {
-  #artistPic {
-    width: 300px;
-    height: 260px;
+  .mainDiv {
+    margin: 10px;
   }
-
-  #artistLabelNGenre {
-    text-align: center;
+  .mainDiv {
+    margin-top: 20px;
+    padding: auto;
   }
-
-  #middleDiv {
-    margin-top: 25px;
-  }
-
-  #bottomDiv {
-    margin-top: 10px;
-  }
-
-  .albumImg {
-    margin-right: 10px;
-    width: 100px;
-    height: 100px;
-  }
-
-  .someInfos {
-    font-size: 12px;
-  }
-
-  h4 {
-    font-size: 16px;
-  }
-}
-
-/* Medium devices (tablets, 768px and up) */
-@media only screen and (min-width: 768px) and (max-width: 1024px) {
-  #artistPic {
-    width: 350px;
-    height: 305px;
-  }
-
-  #artistLabelNGenre {
-    text-align: center;
-  }
-
-  #middleDiv {
-    margin-top: 30px;
-  }
-
-  #bottomDiv {
-    margin-top: 15px;
-  }
-
-  .albumImg {
-    margin-right: 20px;
-    width: 125px;
-    height: 125px;
-  }
-
-  .someInfos {
-    font-size: 14px;
-  }
-
-  h4 {
-    font-size: 18px;
-  }
-}
-
-/* Large devices (desktops, 992px and up) */
-@media only screen and (min-device-width: 1281px) {
-}
 </style>
