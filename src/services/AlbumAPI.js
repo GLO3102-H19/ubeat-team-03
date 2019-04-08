6585477f705a251221083849b33627d6a313bf02
Mathieu <mathieu.kirouac.3@ulaval.ca>
2019-04-08 15:01:27 -0400

    Errors in ArtistAPI & PlaylistAPI handled with alert (to modify)

--- src/services/AlbumAPI.js
+++ src/services/AlbumAPI.js
@@ -2,48 +2,52 @@ import axios from 'axios';
 
 const baseURL = 'http://ubeat.herokuapp.com/unsecure/albums/';
 
-const checkIfAlbumTpeIsAlbum = function (response) {
-  if (response.data.results[0].collectionType === 'Album') {
-    alert('Erreur');
-    throw Error(`${response.statusText} - ${response.baseURL}`);
-  }
-  console.log('Works');
-  return response;
-};
+// Changer les alerts pour quelque chose de mieux
 
 export const getAlbumTitle = albumId => axios.get(baseURL + albumId)
-  .then(checkIfAlbumTpeIsAlbum)
+  // .then(checkStatus)
   .then(response => response.data.results[0].collectionName)
   .catch((error) => {
+    alert(`${error.response.status} ${error.response.statusText}`);
     throw error;
   });
 
 export const getAlbumCover = albumId => axios.get(baseURL + albumId)
+  // .then(checkStatus)
   .then(response => response.data.results[0].artworkUrl100)
   .catch((error) => {
+    alert(`${error.response.status} ${error.response.statusText}`);
     throw error;
   });
 
 export const getAlbumReleaseDate = albumId => axios.get(baseURL + albumId)
+  // .then(checkStatus)
   .then(response => response.data.results[0].releaseDate)
   .catch((error) => {
+    alert(`${error.response.status} ${error.response.statusText}`);
     throw error;
   });
 
 export const getAlbumGenre = albumId => axios.get(baseURL + albumId)
+  // .then(checkStatus)
   .then(response => response.data.results[0].primaryGenreName)
   .catch((error) => {
+    alert(`${error.response.status} ${error.response.statusText}`);
     throw error;
   });
 
 export const getAlbumAppleLink = albumId => axios.get(baseURL + albumId)
+  // .then(checkStatus)
   .then(response => response.data.results[0].collectionViewUrl)
   .catch((error) => {
+    alert(`${error.response.status} ${error.response.statusText}`);
     throw error;
   });
 
 export const getAlbumTracks = albumId => axios.get(`${baseURL + albumId}/tracks`)
+  // .then(checkStatus)
   .then(response => response.data.results)
   .catch((error) => {
+    alert(`${error.response.status} ${error.response.statusText}`);
     throw error;
   });
